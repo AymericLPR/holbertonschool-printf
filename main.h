1c67aac9380242ddd8a8198cf446c6fbaa2c469b
AymericLPR <mric31400@gmail.com>
2022-11-07 09:14:35 -0800

    main printf

--- main.h
+++ main.h
@@ -7,11 +7,24 @@
 #include <limits.h>
 #include <stdarg.h>
 
+/**
+ * struct structure - structure
+ * @valid: character
+ * @f: function
+ */
+
+typedef struct structure
+{
+char *valid;
+int (*f)(va_list);
+} spec;
+
 int _printf(const char *format, ...);
 int print_c(va_list args);
 int print_s(va_list args);
 int print_d(va_list args);
 int print_i(va_list args);
 int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
 
 #endif
