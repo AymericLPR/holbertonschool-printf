f79485e7295c4881db34748f8eaf22e165891ff3
AymericLPR <mric31400@gmail.com>
2022-11-08 07:05:34 -0800

    main printf

--- main.h
+++ main.h
@@ -24,7 +24,6 @@ int print_c(va_list args);
 int print_s(va_list args);
 int print_d(va_list args);
 int print_i(va_list args);
-int print_percent(va_list args);
 int (*get_func(char x))(va_list args);
 
 #endif
