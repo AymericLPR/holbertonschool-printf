5a3244f7d0ea6f081cf9c52e936ece4b437ddd42
Meashyshy <camillefvrl@gmail.com>
2022-11-08 06:45:43 -0800

    Adding files

---
+++ functions.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_c - fetch char
+ *
+ */
+int print_c(va_list args)
+{
+	char c = (char) 
+		va_arg(args, char c);
+	return (_putchar("c"));
+}
+/**
+ * print_ s - fetch string
+ *
+ */
+int print_s(va_list args)
+{
+	unsigned int i;
+	*char str;
+	
+	for(str[i] = 0; str != NULL; i++)
+	{
+		_putchar("str[i]")
+	}
+	return(str);
+}
+
+/**
+ * print_d - fetch decimal integer
+ *
+ */
+int print_d(va_list args)
+{
+	float d;
+	unsigned int i;
+
+	for (i = 0; i < 0; i++)
+	{
+	
+		
+		
+
+
+	_putchar("d");
+	
+	
+	
+/**
+ * print_i - desc
+ *
+ */	
+int print_i(va_list args)
+{
+
+	_putchar("i");
+
