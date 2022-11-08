e96f92871d049734dbf792e308dd952317023588
AymericLPR <mric31400@gmail.com>
2022-11-08 07:02:32 -0800

    adding files

--- functions.c
+++ functions.c
@@ -1,31 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * print_c - fetch char
- *
+ * print_c - prints character
+ * @args: character argument
+ * Return: number of characters
  */
+
 int print_c(va_list args)
 {
-	char c = (char) 
-		va_arg(args, char c);
-	return (_putchar("c"));
+char c;
+c = va_arg(args, char);
+return (_putchar(c));
 }
+
 /**
- * print_ s - fetch string
- *
+ * print_s - prints a string
+ * @args: string  argument
+ * Return: number of characters
  */
+
 int print_s(va_list args)
 {
-	unsigned int i;
-	*char str;
-	
-	for(str[i] = 0; str != NULL; i++)
-	{
-		_putchar("str[i]")
-	}
-	return(str);
+int i, number = 0;
+char *str;
+i = 0;
+str = va_arg(args, char*);
+if (str == NULL)
+str = "(null)";
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+number++;
+}
+return (number);
 }
 
 /**
