5ce8fe54cb0b5fbe3eeb5b8089bd008d5af12e09
AymericLPR <mric31400@gmail.com>
2022-11-09 04:52:43 -0800

    main printf

--- main.h
+++ main.h
@@ -17,12 +17,13 @@ typedef struct structure
 {
 char *valid;
 int (*f)(va_list);
-} spec;
+} struct;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list args);
 int print_s(va_list args);
+int print_percent(va_list args);
 int print_d(va_list args);
 int print_i(va_list args);
 int (*get_func(char x))(va_list args);
