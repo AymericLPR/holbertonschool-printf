0ffcde4c36f33ddc52abc85c9f7decb36105f743
AymericLPR <mric31400@gmail.com>
2022-11-09 04:39:19 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _printf - printf function
@@ -8,42 +8,42 @@ include "main.h"
 
 int _printf(const char *format, ...)
 {
-  if (format != NULL)
-    {
       int (*narg)(va_list);
-      va_list args;
-      va_start(args, format);
       int number = 0;
       int i = 0;
+      va_list args;
 
-      while (format != NULL && format[i] != '\0')
+      va_start(args, format);
+      if (format != NULL)
 	{
-	  if (format[i] == '%')
+	  while (format != NULL && format[i] != '\0')
 	    {
-	      if (format[i + 1] == '%')
+	      if (format[i] == '%')
 		{
-		  _putchar('%');
-		  number++;
-		  i += 2;
+		  if (format[i + 1] == '%')
+		    {
+		      _putchar('%');
+		      number++;
+		      i += 2;
+		    }
+		  else
+		    {
+		      narg = get_func(format[i + 1]);
+		      if (narg)
+			number += narg(args);
+		      else
+			number = _putchar(format[i]) + _putchar(format[i + 1]);
+		      i += 2;
+		    }
 		}
 	      else
 		{
-		  narg = get_func(format[i + 1]);
-		  if (narg)
-		    number += narg(args);
-		  else
-		    number = _putchar(format[i]) + _putchar(format[i + 1]);
-		  i += 2;
+		  number += _putchar(format[i]);
+		  i++;
 		}
 	    }
-	  else
-	    {
-	      number += _putchar(format[i]);
-	      i++;
-	    }
+	  va_end(args);
+	  return (number);
 	}
-      va_end(args);
-      return (number);
-    }
-  return (-1);
+      return (-1);
 }
