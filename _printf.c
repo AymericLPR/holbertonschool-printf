ccd93164a9720d695f72df2b854229776cb528bd
AymericLPR <mric31400@gmail.com>
2022-11-09 04:46:12 -0800

    function printf

--- _printf.c
+++ _printf.c
@@ -8,42 +8,41 @@
 
 int _printf(const char *format, ...)
 {
-      int (*narg)(va_list);
-      int number = 0;
-      int i = 0;
-      va_list args;
+int (*narg)(va_list);
+int number = 0;
+int i = 0;
+va_list args;
 
-      va_start(args, format);
-      if (format != NULL)
-	{
-	  while (format != NULL && format[i] != '\0')
-	    {
-	      if (format[i] == '%')
-		{
-		  if (format[i + 1] == '%')
-		    {
-		      _putchar('%');
-		      number++;
-		      i += 2;
-		    }
-		  else
-		    {
-		      narg = get_func(format[i + 1]);
-		      if (narg)
-			number += narg(args);
-		      else
-			number = _putchar(format[i]) + _putchar(format[i + 1]);
-		      i += 2;
-		    }
-		}
-	      else
-		{
-		  number += _putchar(format[i]);
-		  i++;
-		}
-	    }
-	  va_end(args);
-	  return (number);
-	}
-      return (-1);
+va_start(args, format);
+if (format != NULL)
+{
+while (format != NULL && format[i] != '\0')
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '%')
+{
+number += _putchar(format[i]);
+i += 2;
+}
+else
+{
+narg = get_func(format[i + 1]);
+if (narg)
+number += narg(args);
+else
+number = _putchar(format[i]) + _putchar(format[i + 1]);
+i += 2;
+}
+}
+else
+}
+number += _putchar(format[i]);
+i++;
+}
+}
+va_end(args);
+return (number);
+}
+return (-1);
 }
